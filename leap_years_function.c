7e96771650f38aabc1f3d430b8e2098ba72a5ef7
XandeBarros <barrosalex6@gmail.com>
2021-05-14 16:47:07 -0300

    resolvendo o exercício dos anos bissextos de uma maneira melhor

---
+++ leap_years_function.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int valida(year) {
+  int valid4, valid100, valid400;
+
+  valid4 = !((year % 4) == 0);
+  valid100 = !((year % 100) == 0);
+  valid400 = !((year % 400) == 0);
+
+  return (valid400) || (valid4 && !valid100);
+}
+
+int main() {
+  int yearmin, yearmax; 
+
+  scanf("%d %d", &yearmin, &yearmax);
+
+  while(valida(yearmin)) {
+    ++yearmin;
+  }
+
+  while (yearmax >= yearmin) {
+    if(valida(yearmin)) {
+      printf(" %d", yearmin);
+    }
+
+    yearmin += 4;
+  } 
+}
