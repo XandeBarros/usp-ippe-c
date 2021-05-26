30b6b343d5ce7a5649be48d9ea7449f9fcdaa9a0
XandeBarros <barrosalex6@gmail.com>
2021-05-26 01:20:21 -0300

    runcodes somando dois vetores

---
+++ runcodes/runcodes_sum_vector.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  int arA[5], arB[5], arS[5];
+
+  for(int i = 0; i < 5; i++) {
+    scanf("%d", &arA[i]);
+  }
+
+  for(int i = 0; i < 5; i++) {
+    scanf("%d", &arB[i]);
+  }
+
+  for(int i = 0; i < 5; i++) {
+    arS[i] = arA[i] + arB[i];
+  }
+
+   for(int i = 0; i < 5; i++) {
+    printf("%d ", arS[i]);
+  }
+}
