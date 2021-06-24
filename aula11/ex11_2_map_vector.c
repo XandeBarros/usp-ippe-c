9ef3cfcb85d3f2d728f18201742efc79027be174
XandeBarros <barrosalex6@gmail.com>
2021-06-24 18:15:48 -0300

    Exercícios Aula 11

--- aula11/ex11_2_map_vector.c
+++ aula11/ex11_2_map_vector.c
@@ -1,11 +1,7 @@
 #include <stdio.h> 
 
-int main() {
-  int vector[20], newVector[20];
-
-  for(int i = 0; i < 20; i++) {
-    scanf("%d", &vector[i]);
-  }
+int newVectorLength(int vector[]) {
+  int vectorLength = 0;
 
   for(int i = 0; i < 20; i++) {
     int bool = 0;
@@ -18,9 +14,40 @@ int main() {
       }
     }
 
-    if(bool == 0) {
-      newVector[i] = vector[i];
-      printf("%d ", newVector[i]);
+    if(!bool) {
+      vectorLength++;
     }
+  }
+
+  return vectorLength;
+}
+
+int newVectorValues(int vector[], int newVector[]) {
+  int bool = 0;
+  
+  for(int i = 0; i < 20; i++) {
+
+    for(int j = 0; j < 20; j++) {
+      if(i != j) {
+        if(vector[i] == vector[j]) {
+          newVector[bool] = vector[i];
+          bool++;
+        }
+      }
+    }
+  }
+}
+
+int main() {
+  int vector[20], newVector[newVectorLength(vector)];
+
+  for(int i = 0; i < 20; i++) {
+    scanf("%d", &vector[i]);
+  }
+
+  printf("%d", newVectorLength(vector));
+
+  for(int i = 0; i < newVectorLength(vector); i++) {
+    printf("%d ", newVector[i]);
   }
 }
