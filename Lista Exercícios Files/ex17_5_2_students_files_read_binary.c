f56867934c637c3bd8689fe6ea4d6acebf3e6c6a
XandeBarros <barrosalex6@gmail.com>
2021-07-14 16:02:06 -0300

    Lista de Exercicios extras das Aulas 15 e 16

--- Lista Exercícios Files/ex17_5_2_students_files_read_binary.c
+++ Lista Exercícios Files/ex17_5_2_students_files_read_binary.c
@@ -12,26 +12,23 @@ struct Estudantes {
 void readFile(struct Estudantes estudantes[]) {
   FILE *arq;
 
-  arq = fopen("ex3Alunos.txt", "r+");
+  arq = fopen("ex5Alunos.txt", "rb+");
   if(arq==NULL) {
     exit(0);
   }
 
+  fread(estudantes, sizeof(struct Estudantes), 10, arq);
+
   for(int i = 0; i < 10; i++) {
-    fscanf(arq, "%[^\n]", estudantes[i].nome);
     printf("Nome: ");
     printf("%s\n", estudantes[i].nome);
 
-    fscanf(arq, "%f", &estudantes[i].nota1);
     printf("Nota P1: ");
     printf("%f\n", estudantes[i].nota1);
 
-
-    fscanf(arq, "%f", &estudantes[i].nota2);
     printf("Nota P2: ");
     printf("%f\n", estudantes[i].nota2);
 
-    fscanf(arq, "%f\n", &estudantes[i].notaFinal);
     printf("Nota Final: ");
     printf("%f\n", estudantes[i].notaFinal);
   }  
